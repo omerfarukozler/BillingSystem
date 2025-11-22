c5e9804235a5b3a6d1e460a5bb65996d5bd4eeea
omerfarukozler <omerozler8@gmail.com>
2025-11-22 14:36:19 +0300

    Migrations added for Account entity

--- Billing.Infra/Migrations/20251122113527_AddAccountEntity.Designer.cs
+++ Billing.Infra/Migrations/20251122113527_AddAccountEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Billing.Infra.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Billing.Infra.Migrations
 {
     [DbContext(typeof(BillingDbContext))]
-    partial class BillingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251122113527_AddAccountEntity")]
+    partial class AddAccountEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.0");
